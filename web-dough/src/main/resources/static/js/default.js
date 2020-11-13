9606bfe36dee39fa50a970f97c4107fe11fd1a5e
Kenny1573 <13063925092@163.com>
2020-11-13 14:44:27 +0800

    web炸油条

---
+++ web-dough/src/main/resources/static/js/default.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+    $("#image-add").click(function(){
+        alert("dianji");
+    })
+
+    $("#eat").click(function(){
+        alert("dianji");
+    })
+
+});
